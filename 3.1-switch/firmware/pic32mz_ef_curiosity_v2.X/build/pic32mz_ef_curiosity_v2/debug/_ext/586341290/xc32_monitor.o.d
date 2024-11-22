21e59188a07c89ec54a398593292edde0534b2c5
Per3stroika <per3stroika@gmail.com>
2024-11-21 23:51:38 -0300

    switch

--- 3.1-switch/firmware/pic32mz_ef_curiosity_v2.X/build/pic32mz_ef_curiosity_v2/debug/_ext/586341290/xc32_monitor.o.d
+++
@@ -1,2 +0,0 @@
-build/pic32mz_ef_curiosity_v2/debug/_ext/586341290/xc32_monitor.o: \
- ../src/config/pic32mz_ef_curiosity_v2/stdio/xc32_monitor.c
--- 3.2-switch_arreglado/firmware/pic32mz_ef_curiosity_v2.X/build/pic32mz_ef_curiosity_v2/debug/_ext/1360937237/main_pic32mz.o.d
+++
@@ -1,2 +0,0 @@
-build/pic32mz_ef_curiosity_v2/debug/_ext/1360937237/main_pic32mz.o: \
- ../src/main_pic32mz.c
